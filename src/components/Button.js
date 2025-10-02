fdadaecf76872d7256844434eeb32ab39432f6b7
dianannperla <waafirmelyza@hotmail.com>
2025-10-02 16:59:37 +0100

    feat: initial commit for src/components/Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({ color, children }) => {
+  return (
+    <button className={`btn btn-${color}`}>{children}</button>
+  );
+};
+
+export default Button;
